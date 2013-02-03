c37fa0a7174e372bbf1838a9e74f4457d6776ce7
Kimmy Andersson <cyaspik@gmail.com>
2013-02-03 21:49:41 +0100

    Basic bootstrap.

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 
+// Define the FluxCore root directory.
+define('FC_ROOT', __DIR__.'/');
+
+// Require composer autoloader.
+require_once FC_ROOT.'vendor/autoload.php';
+
 // TODO: Setup everything for index or any other abstraction of the system.

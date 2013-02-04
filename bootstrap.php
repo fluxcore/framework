71dd825b3b9c1c0a411107601186d0b58fc1e4f2
Kimmy Andersson <cyaspik@gmail.com>
2013-02-04 09:50:12 +0100

    IoC container

--- bootstrap.php
+++ bootstrap.php
@@ -6,8 +6,11 @@ define('FC_ROOT', __DIR__.'/');
 // Require composer autoloader.
 $autoload = require_once FC_ROOT.'vendor/autoload.php';
 
+// Create application/IoC container.
+$app = new FluxCore\Core\Application;
+
 // Create and register AliasLoader.
-$aliasLoader = new FluxCore\Core\AliasLoader();
-$aliasLoader->register();
+$app['autoload.alias'] = new FluxCore\Core\AliasLoader();
+$app['autoload.alias']->register();
 
 // TODO: Setup everything for index or any other abstraction of the system.

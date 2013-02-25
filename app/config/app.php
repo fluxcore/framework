2a71eb0bf9fb3600c6f0cd740f288e83e2037afa
Kimmy Andersson <cyaspik@gmail.com>
2013-02-25 18:42:18 +0100

    Cleanup & Documentation

--- app/config/app.php
+++ app/config/app.php
@@ -2,7 +2,19 @@
 
 return array(
 
+	// --------------------------------------------------------------------- //
+	// Application Debugging
+	// --------------------------------------------------------------------- //
+	// Specifies whether debugging functionality should be enabled or not.
+	// --------------------------------------------------------------------- //
+
 	'debug' => true,
+
+	// --------------------------------------------------------------------- //
+	// Application Services
+	// --------------------------------------------------------------------- //
+	// Specifies which services to register to the application.
+	// --------------------------------------------------------------------- //
 
 	'services'	=> array(
 		'FluxCore\Routing\RoutingServiceProvider',
@@ -10,18 +22,32 @@ return array(
 		'Illuminate\Database\DatabaseServiceProvider',
 	),
 
+	// --------------------------------------------------------------------- //
+	// Application Autoload Paths
+	// --------------------------------------------------------------------- //
+	// Specifies which directories that should be searched by the instant
+	// ClassLoader.
+	// --------------------------------------------------------------------- //
+
 	'autoload' => array(
 		__DIR__.'/../controller',
 		__DIR__.'/../model',
 	),
 
+	// --------------------------------------------------------------------- //
+	// Application Class Aliases
+	// --------------------------------------------------------------------- //
+	// Specifies which global class aliases should be provided to the
+	// application.
+	// --------------------------------------------------------------------- //
+
 	'aliases'	=> array(
-		'App' => 'Illuminate\Support\Facades\App',
-		'Route' => 'Illuminate\Support\Facades\Route',
-		'Event' => 'Illuminate\Support\Facades\Event',
-		'ExceptionHandler' => 'FluxCore\Facade\Exception',
-		'View' => 'Illuminate\Support\Facades\View',
-		'Config' => 'Illuminate\Support\Facades\Config',
+		'ExceptionHandler'	=> 'FluxCore\Facade\Exception',
+		'App'				=> 'Illuminate\Support\Facades\App',
+		'Config'			=> 'Illuminate\Support\Facades\Config',
+		'Event'				=> 'Illuminate\Support\Facades\Event',
+		'Route'				=> 'Illuminate\Support\Facades\Route',
+		'View'				=> 'Illuminate\Support\Facades\View',
 	),
 	
 );
--- public/index.php
+++ public/index.php
@@ -1,12 +1,29 @@
 <?php
 
+// ------------------------------------------------------------------------- //
+// FluxCore Start Constant
+// ------------------------------------------------------------------------- //
+// This can be used by profilers to get the time of execution.
+// ------------------------------------------------------------------------- //
+
 define('FLUXCORE_START', microtime(true));
+
+// ------------------------------------------------------------------------- //
+// FluxCore Root Directory
+// ------------------------------------------------------------------------- //
+// This is used to require all bootstrap files.
+// ------------------------------------------------------------------------- //
+
 define('FLUXCORE_ROOT', __DIR__.'/../');
+
+// ------------------------------------------------------------------------- //
+// Bootstrap.
+// ------------------------------------------------------------------------- //
 
 // Require composer autoload.
 require_once FLUXCORE_ROOT.'/vendor/autoload.php';
 
-// Pass bootstrap on to app.
+// Bootstrap application.
 $app = require_once FLUXCORE_ROOT.'/app/start/start.php';
 
 // Run app.

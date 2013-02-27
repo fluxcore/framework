ab89ea149b0f621c2530ac7c46c3fef30038f301
Kimmy Andersson <cyaspik@gmail.com>
2013-02-27 10:36:28 +0100

    Documented asset config.

--- app/config/asset.php
+++ app/config/asset.php
@@ -2,7 +2,23 @@
 
 return array(
 
+	// --------------------------------------------------------------------- //
+	// Asset Public URL Override
+	// --------------------------------------------------------------------- //
+	// When this value is set to anything other than an empty string, it
+	// will override the URL that Asset::pub(...) will use to construct
+	// full asset URLs.
+	// --------------------------------------------------------------------- //
+
 	'public' => '',
+
+	// --------------------------------------------------------------------- //
+	// Asset CDNs
+	// --------------------------------------------------------------------- //
+	// Here you can specify any CDNs that the asset manager should index.
+	// These can later be used to construct asset URLs as following:
+	// Asset::cdn('identifier', 'path/to/file.css');
+	// --------------------------------------------------------------------- //
 
 	'cdn' => array(
 
